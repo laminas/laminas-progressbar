9689a68a6e04c045b6769c1740075e94d4a78409
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:38:30 -0600

    Rewriting as Laminas Project package

--- src/Adapter/Exception/ExceptionInterface.php
+++ src/Adapter/Exception/ExceptionInterface.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-progressbar for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-progressbar/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-progressbar/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\ProgressBar\Adapter\Exception;
+namespace Laminas\ProgressBar\Adapter\Exception;
 
-use Zend\ProgressBar\Exception\ExceptionInterface as ProgressBarException;
+use Laminas\ProgressBar\Exception\ExceptionInterface as ProgressBarException;
 
 /**
- * Exception class for Zend\ProgressBar\Adapter
+ * Exception class for Laminas\ProgressBar\Adapter
  */
 interface ExceptionInterface extends ProgressBarException
 {
