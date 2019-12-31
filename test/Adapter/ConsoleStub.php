8c2fb34df0b0b3644394ca930246fb6cb1b98da6
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:38:41 -0600

    Rewriting as Laminas Project package

--- test/Adapter/ConsoleStub.php
+++ test/Adapter/ConsoleStub.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-progressbar for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-progressbar/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-progressbar/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\ProgressBar\Adapter;
+namespace LaminasTest\ProgressBar\Adapter;
 
-use Zend\ProgressBar\Adapter;
+use Laminas\ProgressBar\Adapter;
 
 class ConsoleStub extends Adapter\Console
 {
